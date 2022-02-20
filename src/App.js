4e184eb901a2a419c563314a60cef82981b70530
jfrumk <jafrumkes@gmail.com>
2022-02-20 13:36:29 -0500

    Update-git

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import './App.css';
-import HeroSection from './components/HeroSection';
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HeroSection from "./components/HeroSection";
 function App() {
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Switch>
-        <Route path='/' exact component= {HeroSection}/>
-      </Switch>
-    </Router>
+      <Navbar />
+      <Router>
+        <Switch>
+          <Route path="/" exact component={HeroSection} />
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,71 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import sheep_logo from '../vectors/sheep_logo.svg';
-import './Navbar.css';
-function Navbar(){
-    const [click, setClick] = useState(false);
+import React, { useEffect, useState } from "react";
+import classes from "../styles/Navbar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
-    return(
-        <>
-            <nav className='navbar'>
-                <div className= 'navbar-container'>
-                    <Link to='/' className='navbar-logo' onClick = {closeMobileMenu}>
-                        <img src={sheep_logo} alt='sheep logo' height='42x' width='51px'/>Jack<br/>Frumkes
-                    </Link>
-                    <div className= {click? 'menu-icon open' : 'menu-icon'} onClick={handleClick}>
-                        <div className='hamburger-icon'></div>
-                    </div>
-                        <ul className = {click ? 'nav-menu active' : 'nav-menu'}> 
-                            <li className = 'nav-item'> 
-                                <Link to = '/' className='nav-links' onClick={closeMobileMenu}> 
-                                    Home
-                                </Link>
-                            </li>
-                            <li className = 'nav-item'>
-                                <Link to = '/contact' className='nav-links' onClick={closeMobileMenu}> 
-                                    Contact
-                                </Link>
-                            </li>
-                            <li className = 'nav-item'>
-                                <Link to = '/about-me' className='nav-links' onClick={closeMobileMenu}> 
-                                    About Me
-                                </Link>
-                            </li>
-                            <li className = 'nav-item'>
-                                <Link to = '/skills' className='nav-links' onClick={closeMobileMenu}> 
-                                    Skills
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-            </nav>
-        </>
-    );
-}
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+    console.log(`${menuOpen} ${size.width}`);
+  };
+  return (
+    <header className={classes.header}>
+      <div className={classes.header__content}>
+        <h2 className={classes.header__content__logo}>JF</h2>
+        <nav
+          className={`${classes.header__content__nav} ${
+            menuOpen && size.width < 768 ? classes.isMenu : ""
+          }`}
+        >
+          <ul>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+            <li>
+              <a href="/">About Me</a>
+            </li>
+            <li>
+              <a href="/">Skills</a>
+            </li>
+          </ul>
+        </nav>
+        <div
+          className={`${classes.header__content__toggle} ${
+            menuOpen ? classes.menuOpen : ""
+          }`}
+        >
+          <FontAwesomeIcon
+            icon={menuOpen ? faTimes : faBars}
+            onClick={menuToggleHandler}
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Navbar;
